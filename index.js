fa1ac68d181d214eacb5a3d6b40b451d051537ae
Ritwik Banerjee <ritwik.banerjee@allianz.de>
2020-05-08 22:23:40 +0200

    catch exceptions for debuggin on the the cloud

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express'),
-      morgan = require('morgan'),
-      helmet = require('helmet'),
-      cors = require('cors'),
-      compression = require('compression');
+  morgan = require('morgan'),
+  helmet = require('helmet'),
+  cors = require('cors'),
+  compression = require('compression');
 
 require('dotenv').config();
 
@@ -16,7 +16,8 @@ app.use(express.json());
 
 
 app.post('/', async (req, res) => {
-    console.log('REQUEST URL: ', req.body.url);
+  console.log('REQUEST URL: ', req.body.url);
+  try {
     if (req.body.url) {
       const pdf = await printPDF(req.body.url);
       res.set({ 'Content-Type': 'application/pdf', 'Content-Length': pdf.length });
@@ -24,23 +25,32 @@ app.post('/', async (req, res) => {
     } else {
       res.status(500).send();
     }
-    
+  } catch(err) {
+    res.status(err);
+  }
+  
+
 })
 
 async function printPDF(url) {
+  try {
     const puppeteer = require('puppeteer');
     const browser = await puppeteer.launch({ headless: false });
     const page = await browser.newPage();
-    await page.goto(url, {waitUntil: 'networkidle0'});
+    await page.goto(url, { waitUntil: 'networkidle0' });
     // await page.emulateMedia('screen');
     const pdf = await page.pdf({ format: 'A4' });
-   
+
     await browser.close();
     return pdf
+  } catch (err) {
+    return err
   }
+
+}
 
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log(`Listening at http://localhost:${port}`);
-  });
+  console.log(`Listening at http://localhost:${port}`);
+});
