640f4d3849003633f7e989a61b7c820da6f63744
Ritwik Banerjee <ritwik.banerjee@allianz.de>
2020-05-08 22:04:35 +0200

    set up a basic print server

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require('express'),
+      morgan = require('morgan'),
+      helmet = require('helmet'),
+      cors = require('cors'),
+      compression = require('compression');
+
+require('dotenv').config();
+
+const app = express();
+
+app.use(compression());
+app.use(morgan('common'));
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+
+
+app.post('/', async (req, res) => {
+    console.log('URL: ', req.body.url);
+    const pdf = await printPDF(req.body.url);
+    res.set({ 'Content-Type': 'application/pdf', 'Content-Length': pdf.length });
+    res.send(pdf);
+})
+
+async function printPDF(url) {
+    const puppeteer = require('puppeteer');
+    const browser = await puppeteer.launch({ headless: true });
+    const page = await browser.newPage();
+    await page.goto(url, {waitUntil: 'networkidle0'});
+    // await page.emulateMedia('screen');
+    const pdf = await page.pdf({ format: 'A4' });
+   
+    await browser.close();
+    return pdf
+  }
+
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Listening at http://localhost:${port}`);
+  });
