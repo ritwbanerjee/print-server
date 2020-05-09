c5e2fdf97af773b6eaa12d9f7b0c37ba95d54dd9
Ritwik Banerjee <ritwik.banerjee@allianz.de>
2020-05-09 02:34:08 +0200

    try catch for puppetter launch

--- controller/print-file.js
+++ controller/print-file.js
@@ -2,25 +2,30 @@
 module.exports = {
 
     createPDF: async function (res, url) {
-        const puppeteer = require('puppeteer');
-        const browser = await puppeteer.launch({
-            headless: true,
-            args: ['--no-sandbox', '--disable-setuid-sandbox']
-        });
-        const page = await browser.newPage();
-        await page.goto(url, { waitUntil: 'networkidle0' });
-
-        const buffer = await page.pdf({
-            format: "A4",
-            printBackground: true,
-            margin: {
-                left: "0px",
-                top: "0px",
-                right: "0px",
-                bottom: "0px"
-            }
-        });
-        await browser.close();
-        res.end(buffer);
+        try {
+            const puppeteer = require('puppeteer');
+            const browser = await puppeteer.launch({
+                headless: true,
+                args: ['--no-sandbox', '--disable-setuid-sandbox']
+            });
+            const page = await browser.newPage();
+            await page.goto(url, { waitUntil: 'networkidle0' });
+    
+            const buffer = await page.pdf({
+                format: "A4",
+                printBackground: true,
+                margin: {
+                    left: "0px",
+                    top: "0px",
+                    right: "0px",
+                    bottom: "0px"
+                }
+            });
+            await browser.close();
+            res.end(buffer);
+        } catch(err) {
+            return err
+        }
+        
     }
 };
