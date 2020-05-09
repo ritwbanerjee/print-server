0c7e9df04e98581aa196f60dc14ba92b3d78a878
Ritwik Banerjee <ritwik.banerjee@allianz.de>
2020-05-09 02:23:29 +0200

    fix file lint

--- controller/print-file.js
+++ controller/print-file.js
@@ -1,26 +1,26 @@
-const puppeteer = require('puppeteer');
 
 module.exports = {
-    
-    createPDF: async function(res, url) {
 
-        console.log('URL: ', url);
+    createPDF: async function (res, url) {
+        const puppeteer = require('puppeteer');
+        const browser = await puppeteer.launch({
+            headless: true,
+            args: ['--no-sandbox', '--disable-setuid-sandbox']
+        });
+        const page = await browser.newPage();
+        await page.goto(url, { waitUntil: 'networkidle0' });
 
-      const browser = await puppeteer.launch({ headless: true });
-      const page = await browser.newPage();
-      await page.goto(url, { waitUntil: 'networkidle0' });
-
-      const buffer = await page.pdf({
-        format: "A4",
-        printBackground: true,
-        margin: {
-          left: "0px",
-          top: "0px",
-          right: "0px",
-          bottom: "0px"
-        }
-      });
-      await browser.close();
-      res.end(buffer);
+        const buffer = await page.pdf({
+            format: "A4",
+            printBackground: true,
+            margin: {
+                left: "0px",
+                top: "0px",
+                right: "0px",
+                bottom: "0px"
+            }
+        });
+        await browser.close();
+        res.end(buffer);
     }
-  };
+};
